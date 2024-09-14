f876dcf21127fc5575cb46d1bb683cb79ebf5679
leolixingyou <leesungwoo0501@naver.com>
2024-09-15 04:27:43 +0900

    remove remote build/ and devel/

--- build/sensing/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/src/sensing
-# Build directory: /workspace/build/sensing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
