c49aadd298184878a3480c5237268cd9bb34a20c
leolixingyou <leesungwoo0501@naver.com>
2024-09-22 01:05:17 +0000

    launch with code by using launch generator

--- src/boss/src/launch_generator.py
+++ src/boss/src/launch_generator.py
@@ -3,48 +3,95 @@ import rospy
 import signal
 import sys
 
-class NodeLauncher:
-    def __init__(self):
-        rospy.init_node('launch_node', anonymous=False)
-        self.launch = roslaunch.scriptapi.ROSLaunch()
-        self.launch.start()
-        self.processes = []
+class Node_Launcher_Generator:
+    def __init__(self, nodes_properties_dict):
+        rospy.init_node('Node_Launcher_Generator', anonymous=False)
+        self.processes = None
+        self.launcher = roslaunch.scriptapi.ROSLaunch()
+        self.launcher.start()
+        self.nodes_stand_by_dict = self.launch_node_setting(nodes_properties_dict)
 
-    def launch_nodes(self):
-        # 配置重要节点
-        node1 = roslaunch.core.Node(
-            package='boss',
-            node_type='ros_boss_node.py',
-            name='Boss_Sever_1',
-            required=True
+    def nodes_properties_setting(self,node_properties):
+        # Node setting
+        node = roslaunch.core.Node(
+            package = node_properties['package'],
+            node_type = node_properties['node_type'],
+            name = node_properties['name'],
+            required = node_properties['required'],
+            output = node_properties['output']
         )
-        # 配置可选节点
-        node2 = roslaunch.core.Node(
-            package='your_package',
-            node_type='optional_node',
-            name='optional_node'
-        )
-        # 配置自动重启的节点
-        node3 = roslaunch.core.Node(
-            package='your_package',
-            node_type='another_node',
-            name='another_node',
-            respawn=True,
-            respawn_delay=5
-        )
+        return node
 
-        # 启动节点
-        self.processes.append(self.launch.launch(node1))
-        # self.processes.append(self.launch.launch(node2))
-        # self.processes.append(self.launch.launch(node3))
+    def launch_nodes(self, node):
+        launch_process = self.launcher.launch(node)
+        return launch_process
+
+    def launch_node_setting(self, nodes_properties_dict) -> dict:
+        """
+        @param package: node package name
+        @type  package: str
+
+        @param node_type: node type
+        @type  node_type: str
+        
+        @param name: node name
+        @type  name: str
+        
+        @param namespace: namespace for node
+        @type  namespace: str
+        
+        @param machine_name: name of machine to run node on
+        @type  machine_name: str
+        
+        @param args: argument string to pass to node executable
+        @type  args: str
+        
+        @param respawn: if True, respawn node if it dies
+        @type  respawn: bool
+        
+        @param remap_args: list of [(from, to)] remapping arguments
+        @type  remap_args: [(str, str)]: 
+        
+        @param env_args: list of [(key, value)] of
+        additional environment vars to set for node
+        @type  env_args: [(str, str)]
+        
+        @param output: where to log output to, either Node, 'screen' or 'log'
+        @type  output: str
+        
+        @param cwd: current working directory of node, either 'node', 'ROS_HOME' or 'ros-root'. Default: ROS_HOME
+        @type  cwd: str
+        
+        @param launch_prefix: launch command/arguments to prepend to node executable arguments
+        @type  launch_prefix: str
+        
+        @param required: node is required to stay running (launch fails if node dies)
+        @type  required: bool
+        
+        @param filename: name of file Node was parsed from
+        @type  filename: str
+
+        @raise ValueError: if parameters do not validate
+        """        
+
+        return {x:self.nodes_properties_setting(y) for x,y in nodes_properties_dict.items()}
 
     def shutdown(self):
         print("Shutting down all nodes...")
         for process in self.processes:
             if process.is_alive():
                 process.stop()
-        self.launch.stop()
+        self.launcher.stop()
         print("All nodes have been stopped.")
+
+    def run(self):
+        self.processes = []
+        try:
+            self.processes = [self.launch_nodes(x) for x in self.nodes.values()]
+            print("All nodes have been launched. Press Ctrl+C to stop.")
+            rospy.spin()
+        finally:
+            self.shutdown()
 
 def signal_handler(signum, frame):
     print("Received shutdown signal. Stopping all nodes...")
@@ -52,17 +99,36 @@ def signal_handler(signum, frame):
     sys.exit(0)
 
 if __name__ == '__main__':
-    launcher = NodeLauncher()
+    nodes_properties = {
+
+        # 'Boss_Server' : 
+        # {'package':'boss','node_type':'ros_boss_node.py','name':'Boss_Server',
+        # 'required':False,'output':'screen'},
+
+        'Control_Server' : 
+        {'package':'control','node_type':'control.py','name':'Control_Server',
+        'required':False,'output':'screen'},
+        
+        'Sensing_Server' : 
+        {'package':'sensing','node_type':'sensing.py','name':'Sensing_Server',
+        'required':False,'output':'screen'},
+        
+
+        'Perception_Server' : 
+        {'package':'perception','node_type':'perception.py','name':'Perception_Server',
+        'required':False,'output':'screen'},
+        
+
+        'Planning_Server' : 
+        {'package':'planning','node_type':'planning.py','name':'Planning_Server',
+        'required':False,'output':'screen'},
+
+        }
+    
+    
+    launcher = Node_Launcher_Generator(nodes_properties)
     
     # 注册信号处理器
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
-
-    try:
-        launcher.launch_nodes()
-        print("All nodes have been launched. Press Ctrl+C to stop.")
-        rospy.spin()
-    except rospy.ROSInterruptException:
-        pass
-    finally:
-        launcher.shutdown()
+    launcher.run()
