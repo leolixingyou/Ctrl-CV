30cdf1d915b5142c1c4f53f682ae916e2393c2eb
leolixingyou <leesungwoo0501@naver.com>
2024-10-06 08:14:50 +0000

    make control mpc draft

--- src/control/src/control.py
+++ src/control/src/control.py
@@ -2,17 +2,17 @@
 import rospy
 import time
 import cv2
-from geometry_msgs.msg import Twist
+from carla_msgs.msg import CarlaEgoVehicleControl
 
 class Control_Lancher_Manager:
     def __init__(self) -> None:
         rospy.init_node('Control_Server', anonymous=True)
         self.rate = rospy.Rate(1) # 10hz
-        self.pub_twist = rospy.Publisher('/carla/ego_vehicle/control/set_target_velocity', Twist, queue_size=10)
+        self.pub_twist = rospy.Publisher('/carla/ego_vehicle/vehicle_control_cmd', CarlaEgoVehicleControl, queue_size=10)
 
     
     def pub_control(self,):
-        msg = Twist()
+        msg = CarlaEgoVehicleControl()
         msg.linear.x = 0.0
         msg.linear.y = 0.0
         msg.linear.z = 0.0
