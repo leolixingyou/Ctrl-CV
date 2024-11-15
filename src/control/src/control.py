9c6512b18b6d9b1869c5117bd6d8f0fcc54c4157
leolixingyou <leesungwoo0501@naver.com>
2024-11-15 09:57:51 +0000

    Update control with nothing

--- src/control/src/control.py
+++ src/control/src/control.py
@@ -2,32 +2,46 @@
 import rospy
 import time
 import cv2
-from carla_msgs.msg import CarlaEgoVehicleControl
+from global_path_subscriber import EnhancedGlobalPathSubscriber
 
 class Control_Lancher_Manager:
     def __init__(self) -> None:
-        rospy.init_node('Control_Server', anonymous=True)
-        self.rate = rospy.Rate(1) # 10hz
-        self.pub_twist = rospy.Publisher('/carla/ego_vehicle/vehicle_control_cmd', CarlaEgoVehicleControl, queue_size=10)
+        rospy.init_node('Control_Server', anonymous=False)
+        self.global_path_cl = EnhancedGlobalPathSubscriber()
+        self.control_init = False
+        self.global_path = []
 
+
+    def check_reset_signal(self):
+        """
+        TODO 
+        Implement your reset signal check here
+        For example, you could subscribe to a reset topic or check a parameter
+        Return True if reset is requested, False otherwise
+        
+        """
+        return False
     
-    def pub_control(self,):
-        msg = CarlaEgoVehicleControl()
-        msg.linear.x = 0.0
-        msg.linear.y = 0.0
-        msg.linear.z = 0.0
-        msg.angular.x = 0.0
-        msg.angular.y = 0.0
-        msg.angular.z = 0.0
-        self.pub_twist.publish(msg)
+    def control_setting_init(self,):
+        if self.global_path_cl.path_changed == 0:
+            self.global_path = self.global_path_cl.get_waypoints()
+            self.control_init = True
+        return self.global_path
 
     def run(self):
-        cnt = 1
-        while not rospy.is_shutdown() :
-            print('control',cnt)
-            cnt += 1
-            self.pub_control()
-            self.rate.sleep()
+        rate = rospy.Rate(10)  # 10 Hz
+        temp_flag = False
+        while not rospy.is_shutdown():
+            print(self.global_path_cl.get_waypoints())
+            if not self.control_init:
+                global_path = self.control_setting_init()
+            elif self.control_init and not temp_flag:
+                print('start-mpc')
+                self.mpc.run(global_path)
+                temp_flag = True
+            else:
+                print('waiting')
+            
 
 if __name__ == "__main__":
     test_temp = Control_Lancher_Manager()
